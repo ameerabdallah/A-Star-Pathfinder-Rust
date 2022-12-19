5e76afa7062f0c67e39a3dcc001d10eb67f82bd1
Ameer Abdallah <ameeryabdallah1@gmail.com>
2022-12-18 21:40:28 -0800

    Code refactor

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,6 @@ use std::{
     path::Path,
 };
 
-
 fn lines_from_file(filename: impl AsRef<Path>) -> Vec<String> {
     let file = File::open(filename).expect("no such file");
     let buf = BufReader::new(file);
@@ -22,15 +21,12 @@ fn lines_from_file(filename: impl AsRef<Path>) -> Vec<String> {
         .collect()
 }
 
-#[cfg(not(windows))]
-fn main() {
+fn run() {
     control::set_override(true);
     // Read in file
     // read command line arguments for the filename
     let args: Vec<String> = std::env::args().collect();
     
-
-
     if args.len() >= 3 {
         let mut grid: Grid;
 
@@ -64,59 +60,16 @@ fn main() {
         println!("No arguments were given, please try again with arguments in this format.");
         println!("pathfinder [x_size] [y_size] [filename <optional>]");
     }
-
     
     colored::control::unset_override();
 }
 
-
-#[cfg(windows)]
 fn main() {
+    #[cfg(windows)]
     match control::set_virtual_terminal(true){
         Ok(_i) => {},
         Err(_err) => panic!("Couldn't set virtual terminal"),
     }
-    control::set_override(true);
-    // Read in file
-    // read command line arguments for the filename
-    let args: Vec<String> = std::env::args().collect();
-    
 
-
-    if args.len() >= 3 {
-        let mut grid: Grid;
-
-        let x_size;
-        let y_size;
-
-        match args[1].parse::<usize>() {
-            Ok(i) => x_size = i,
-            Err(err) => panic!("{}", err),
-        }
-        match args[2].parse::<usize>() {
-            Ok(i) => y_size = i,
-            Err(err) => panic!("{}", err),
-        }
-
-        if args.len() == 4 {
-            // &args[1] is the filename
-            let map_vec = lines_from_file(&args[3]);
-            grid = Grid::from(map_vec, x_size, y_size);
-        }
-        else {
-            grid = Grid::generate_random_grid(x_size, y_size);
-        }
-        
-        println!("Unsolved Map: ");
-        grid.print_grid();
-        println!("\nSolved Map: ");
-        grid.run_a_star(true);
-    }
-    else {
-        println!("No arguments were given, please try again with arguments in this format.");
-        println!("pathfinder [x_size] [y_size] [filename <optional>]");
-    }
-
-    
-    colored::control::unset_override();
+    run();
 }
